bff211cdea9168ca97e736eb2e1014f5572dbdf7
Darryl Kilzer <darrylkilzer@gmail.com>
2020-11-13 14:06:03 -0700

    t

--- kanbanned.server/server/Startup.js
+++ kanbanned.server/server/Startup.js
@@ -25,7 +25,7 @@ export default class Startup {
             defaultSrc: ["'self'"],
             scriptSrc: ["'self'", `https://${domain}/*`],
             styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
-            imgSrc: ["'self'", `https://${domain}`, 'data:', 'https://bcw.blob.core.windows.net/public/img/*', 'https://avatars0.githubusercontent.com/*'],
+            imgSrc: ["'self'", `https://${domain}`, 'data:', 'https://bcw.blob.core.windows.net/public/img/*', 'https://avatars0.githubusercontent.com'],
             connectSrc: ["'self'", `https://${domain}/oauth/token`, `https://${domain}/userinfo`],
             fontSrc: ["'self'", 'https://fonts.gstatic.com'],
             objectSrc: ["'self'"],
