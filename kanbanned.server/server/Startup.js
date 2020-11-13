2613cec8f9b985c36d0052121e6ed28c952e6cc0
Darryl Kilzer <darrylkilzer@gmail.com>
2020-11-13 14:10:24 -0700

    just allow the images

--- kanbanned.server/server/Startup.js
+++ kanbanned.server/server/Startup.js
@@ -25,7 +25,7 @@ export default class Startup {
             defaultSrc: ["'self'"],
             scriptSrc: ["'self'", `https://${domain}/*`],
             styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
-            imgSrc: ["'self'", `https://${domain}`, 'data:', 'https://boisecodeworks.com', 'https://github.com'],
+            imgSrc: ["'self'", '*'],
             connectSrc: ["'self'", `https://${domain}/oauth/token`, `https://${domain}/userinfo`],
             fontSrc: ["'self'", 'https://fonts.gstatic.com'],
             objectSrc: ["'self'"],
